674d5d023f51dec62be1e5dbbf32b1a216ab917d
wangzijian <2087291150@qq.com>
2024-08-15 12:34:47 +0000

    ort unet code init

---
+++ lite/ort/sd/unet.cpp
@@ -0,0 +1,5 @@
+//
+// Created by root on 8/14/24.
+//
+
+#include "unet.h"
---
+++ lite/ort/sd/unet.h
@@ -0,0 +1,14 @@
+//
+// Created by root on 8/14/24.
+//
+
+#ifndef LITE_AI_TOOLKIT_UNET_H
+#define LITE_AI_TOOLKIT_UNET_H
+
+
+class unet {
+
+};
+
+
+#endif //LITE_AI_TOOLKIT_UNET_H
