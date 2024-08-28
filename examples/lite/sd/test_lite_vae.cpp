6812841609ecf87f71e8bb86274965e666c26c9d
wangzijian <2087291150@qq.com>
2024-08-28 06:25:18 +0000

    add tensorrt vae implement code #TODO clean code

--- examples/lite/sd/test_lite_vae.cpp
+++ examples/lite/sd/test_lite_vae.cpp
@@ -32,10 +32,38 @@ static void test_default()
 }
 
 
+static void test_trt_vae()
+{
+    auto start = std::chrono::high_resolution_clock::now();
+
+    std::string engine_path = "/home/lite.ai.toolkit/examples/hub/trt/vae_model.engine";
+
+
+    lite::trt::sd::image_decoder::Vae *trt_vae = new lite::trt::sd::image_decoder::Vae (engine_path);
+
+
+    std::vector<std::string> input_vector = {"i am not good at cpp","goi ofg go !"};
+
+    std::vector<std::vector<float>> output;
+
+    trt_vae->inference();
+
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+
+    std::cout << "耗时: " << duration << " 毫秒" << std::endl;
+
+
+    delete trt_vae;
+
+}
+
 
 static void test_lite()
 {
-    test_default();
+    test_trt_vae();
+//    test_default();
 }
 
 int main(__unused int argc, __unused char *argv[])
