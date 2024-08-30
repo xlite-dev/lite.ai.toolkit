6e2d68e7074d95cd33e08af422490b3de817dc3d
wangzijian <2087291150@qq.com>
2024-08-30 08:48:21 +0000

    Update the test code of VAE

--- examples/lite/sd/test_lite_vae.cpp
+++ examples/lite/sd/test_lite_vae.cpp
@@ -12,16 +12,14 @@ static void test_default()
 
     lite::onnxruntime::sd::image_decoder::Vae *vae = new lite::onnxruntime::sd::image_decoder::Vae (engine_path);
 
-
-
     std::vector<std::string> input_vector = {"i am not good at cpp","goi ofg go !"};
 
     std::vector<std::vector<float>> output;
 
     vae->inference(input_vector,output);
 
-
     auto end = std::chrono::high_resolution_clock::now();
+
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
 
     std::cout << "耗时: " << duration << " 毫秒" << std::endl;
@@ -34,7 +32,6 @@ static void test_default()
 
 static void test_trt_vae()
 {
-    auto start = std::chrono::high_resolution_clock::now();
 
     std::string engine_path = "/home/lite.ai.toolkit/examples/hub/trt/vae_model_fp16.engine";
 
@@ -46,14 +43,16 @@ static void test_trt_vae()
 
     std::vector<std::vector<float>> output;
 
+
+    auto start = std::chrono::high_resolution_clock::now();
+
     trt_vae->inference();
 
-
     auto end = std::chrono::high_resolution_clock::now();
+
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
 
     std::cout << "耗时: " << duration << " 毫秒" << std::endl;
-
 
     delete trt_vae;
 
@@ -63,7 +62,7 @@ static void test_trt_vae()
 static void test_lite()
 {
     test_trt_vae();
-//    test_default();
+    test_default();
 }
 
 int main(__unused int argc, __unused char *argv[])
