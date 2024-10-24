c53006b5fbb1f8d4beefdb9bfd70d5ad4ee1bf27
wangzijian <2087291150@qq.com>
2024-10-24 10:13:42 +0000

    update code

--- examples/lite/cv/test_lite_realesrgan.cpp
+++ examples/lite/cv/test_lite_realesrgan.cpp
@@ -5,9 +5,9 @@
 
 static void test_default()
 {
-    std::string onnx_path = "/home/lite.ai.toolkit/examples/hub/onnx/cv/RealESRGAN_x4plus.onnx";
-    std::string test_img_path = "/home/lite.ai.toolkit/examples/lite/resources/test_lite_realesrgan.jpg";
-    std::string save_img_path = "/home/lite.ai.toolkit/examples/logs/test_lite_realesrgan.jpg";
+    std::string onnx_path = "../../../examples/hub/onnx/cv/RealESRGAN_x4plus.onnx";
+    std::string test_img_path = "../../../examples/lite/resources/test_lite_realesrgan.jpg";
+    std::string save_img_path = "../../../examples/logs/test_lite_realesrgan.jpg";
 
     // 1. Test Default Engine ONNXRuntime
     lite::cv::upscale::RealESRGAN *realEsrgan = new lite::cv::upscale::RealESRGAN (onnx_path);
