2b080ed9058067a1d3345e1ee27d81c16ecf8bf7
wangzijian <2087291150@qq.com>
2024-11-14 07:08:20 +0000

    tensorrt facefusion code

--- examples/lite/cv/test_lite_face_68landmarks.cpp
+++ examples/lite/cv/test_lite_face_68landmarks.cpp
@@ -6,8 +6,8 @@
 static void test_default()
 {
 #ifdef ENABLE_ONNXRUNTIME
-    std::string onnx_path = "../../../examples/hub/onnx/cv/2dfan4.onnx";
-    std::string test_img_path = "../../../examples/lite/resources/test_lite_facefusion_pipeline_source.jpg";
+    std::string onnx_path = "/home/lite.ai.toolkit/examples/hub/onnx/cv/2dfan4.onnx";
+    std::string test_img_path = "/home/lite.ai.toolkit/examples/lite/resources/test_lite_facefusion_pipeline_source.jpg";
 
     // 1. Test Default Engine ONNXRuntime
     lite::cv::faceid::Face_68Landmarks *face68Landmarks = new lite::cv::faceid::Face_68Landmarks(onnx_path);
@@ -28,8 +28,37 @@ static void test_default()
 #endif
 }
 
+
+
+
+static void test_tensorrt()
+{
+#ifdef ENABLE_TENSORRT
+    std::string engine_path = "/home/lite.ai.toolkit/examples/hub/trt/2dfan4_fp16.engine";
+    std::string test_img_path = "/home/lite.ai.toolkit/1.jpg";
+
+    // 1. Test TensorRT Engine
+    lite::trt::cv::faceid::FaceFusionFace68Landmarks  *face68Landmarks = new lite::trt::cv::faceid::FaceFusionFace68Landmarks(engine_path);
+    lite::types::BoundingBoxType<float, float> bbox;
+    bbox.x1 = 487;
+    bbox.y1 = 236;
+    bbox.x2 = 784;
+    bbox.y2 = 624;
+
+    cv::Mat img_bgr = cv::imread(test_img_path);
+    std::vector<cv::Point2f> face_landmark_5of68;
+    face68Landmarks->detect(img_bgr, bbox, face_landmark_5of68);
+
+    std::cout<<"face id detect done!"<<std::endl;
+
+    delete face68Landmarks;
+#endif
+}
+
+
 int main(__unused int argc, __unused char *argv[])
 {
-    test_default();
+    test_tensorrt();
+//    test_default();
     return 0;
 }
