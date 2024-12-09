38357c7206a410429d9990de79dd12c472b94737
wangzijian <2087291150@qq.com>
2024-12-09 04:03:05 +0000

    update code

--- examples/lite/cv/test_lite_facefusion_pipeline.cpp
+++ examples/lite/cv/test_lite_facefusion_pipeline.cpp
@@ -42,11 +42,11 @@ static void test_default()
 static void test_tensorrt()
 {
 #ifdef ENABLE_TENSORRT
-    std::string face_swap_onnx_path = "/home/lite.ai.toolkit/examples/hub/trt/inswapper_128_fp16.engine";
-    std::string face_detect_onnx_path = "/home/lite.ai.toolkit/examples/hub/trt/yoloface_8n_fp16.engine";
-    std::string face_landmarks_68 = "/home/lite.ai.toolkit/examples/hub/trt/2dfan4_fp16.engine";
-    std::string face_recognizer_onnx_path = "/home/lite.ai.toolkit/examples/hub/trt/arcface_w600k_r50_fp16.engine";
-    std::string face_restoration_onnx_path = "/home/lite.ai.toolkit/examples/hub/trt/gfpgan_1.4_fp32.engine";
+    std::string face_swap_onnx_path = "../../../examples/hub/trt/inswapper_128_fp16.engine";
+    std::string face_detect_onnx_path = "../../../examples/hub/trt/yoloface_8n_fp16.engine";
+    std::string face_landmarks_68 = "../../../examples/hub/trt/2dfan4_fp16.engine";
+    std::string face_recognizer_onnx_path = "../../../examples/hub/trt/arcface_w600k_r50_fp16.engine";
+    std::string face_restoration_onnx_path = "../../../examples/hub/trt/gfpgan_1.4_fp32.engine";
 
     auto pipeLine =  lite::trt::cv::face::swap::FaceFusionPipeLine (
             face_detect_onnx_path,
@@ -56,9 +56,9 @@ static void test_tensorrt()
             face_restoration_onnx_path
     );
 
-    std::string source_image_path = "/home/lite.ai.toolkit/1.jpg";
-    std::string target_image_path = "/home/lite.ai.toolkit/5.jpg";
-    std::string save_image_path = "/home/lite.ai.toolkit/trt_pipeline_result_cuda_test_1.jpg";
+    std::string source_image_path = "../../../examples/logs/1.jpg";
+    std::string target_image_path = "../../../examples/logs/5.jpg";
+    std::string save_image_path = "../../../examples/logs/trt_pipeline_result_cuda_test_13_mt.jpg";
 
 
     // 写一个测试时间的代码
@@ -67,7 +67,7 @@ static void test_tensorrt()
     pipeLine.detect(source_image_path,target_image_path,save_image_path);
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> diff = end-start;
-    std::cout << "Time: " << diff.count() << " s\n";
+    std::cout << "Time: " << diff.count()  * 1000<< " ms\n";
 
 
 #endif
